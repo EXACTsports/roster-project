2994f763d05608f03e611d3775c4c1be31fcabc5
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-14 10:03:24 -0500

    feat: fix script and tailwind extend style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backdropBlur: {
+        xs: '1px',
+      }
+    },
   },
   plugins: [],
 }
