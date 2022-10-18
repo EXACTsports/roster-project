630d7914c2e8b67f54739b269db95adb97989b61
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-18 11:43:57 -0500

    db: add two columns to athletes table

---
+++ database/migrations/2022_10_18_145655_alter_roster_table_height_high_school.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rosters', function (Blueprint $table) {
+            $table->integer('height')->nullable();
+            $table->string('high_school')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rosters');
+    }
+};
